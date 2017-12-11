c56f87c78e1c71ce0264ca36cc4d64821a37a9c3
wc1997 <wilson.cao97@gmail.com>
2017-12-10 22:57:15 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from riotwatcher import RiotWatcher
 
-key = raw_input("Input API key")
-
 watcher = RiotWatcher(key)
+
 challenger = watcher.league.challenger_by_queue("na1", "RANKED_SOLO_5x5")
 print(challenger['tier'])
 
@@ -14,7 +13,8 @@ for key in challenger['entries']:
     challenger_name.append(key['playerOrTeamName'])
 
 
-print(challenger_Ids[1])
-print(challenger_name[1])
+#print(challenger_Ids[1])
+#print(challenger_name[1])
+#print(watcher.summoner.by_id("na1", challenger_Ids[1])['accountId'])
 
-print(watcher.match.matchlist_by_account_recent("na1", watcher.summoner.by_id(challenger_Ids[1])))
+print(watcher.match.matchlist_by_account_recent("na1",watcher.summoner.by_id("na1", challenger_Ids[1])['accountId']))
