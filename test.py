c70958982b03704ca9d6138b7ddaa65a9ccdc459
wc1997 <wilson.cao97@gmail.com>
2017-12-10 22:47:15 -0800

    Update test.py
    Further testing of scrapping data from API

--- test.py
+++ test.py
@@ -2,9 +2,19 @@ from riotwatcher import RiotWatcher
 
 key = raw_input("Input API key")
 
-
 watcher = RiotWatcher(key)
 challenger = watcher.league.challenger_by_queue("na1", "RANKED_SOLO_5x5")
 print(challenger['tier'])
+
+challenger_Ids = list()
+challenger_name = list()
+challenger_accountIds = list()
 for key in challenger['entries']:
-	print(key['playerOrTeamName'])
+    challenger_Ids.append(key['playerOrTeamId'])
+    challenger_name.append(key['playerOrTeamName'])
+
+
+print(challenger_Ids[1])
+print(challenger_name[1])
+
+print(watcher.match.matchlist_by_account_recent("na1", watcher.summoner.by_id(challenger_Ids[1])))
