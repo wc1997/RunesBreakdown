8d6d96a4d827531e043626980365d9934946e0e9
wc1997 <wilson.cao97@gmail.com>
2017-12-13 14:05:12 -0800

    Update test.py
    Added data grabbed from riot's api to a dataframe to reduce number of calls in the future. Instead I can reference "master_and_challenger.csv" for data I've already collected.  Left some commented out code for future reference.

--- test.py
+++ test.py
@@ -1,20 +1,62 @@
 from riotwatcher import RiotWatcher
+import pandas as pd
+import numpy as np
+
+
+key = raw_input("Input API key")
+
+region = "na1"
 
 watcher = RiotWatcher(key)
+challenger = watcher.league.challenger_by_queue(region, "RANKED_SOLO_5x5")
+masters = watcher.league.masters_by_queue(region, "RANKED_SOLO_5x5")
 
-challenger = watcher.league.challenger_by_queue("na1", "RANKED_SOLO_5x5")
 print(challenger['tier'])
 
 challenger_Ids = list()
-challenger_name = list()
+challenger_names = list()
 challenger_accountIds = list()
+
+masters_Ids = list()
+masters_names = list()
+masters_accountIds = list()
+
 for key in challenger['entries']:
     challenger_Ids.append(key['playerOrTeamId'])
-    challenger_name.append(key['playerOrTeamName'])
+    challenger_names.append(key['playerOrTeamName'])
+
+for key in masters['entries']:
+    masters_Ids.append(key['playerOrTeamId'])
+    masters_names.append(key['playerOrTeamName'])
+
+c = {"ids": challenger_Ids, "name": challenger_names, "rank": "challenger"}
+m = {"ids": masters_Ids, "name": masters_names, "rank": "master"}
+
+dfc = pd.DataFrame(data=c)
+dfm = pd.DataFrame(data=m)
+
+frames = [dfc, dfm]
+result = pd.concat(frames)
+print(result)
+
+result.to_csv('masters_and_challengers.csv')
 
 
-#print(challenger_Ids[1])
-#print(challenger_name[1])
-#print(watcher.summoner.by_id("na1", challenger_Ids[1])['accountId'])
 
-print(watcher.match.matchlist_by_account_recent("na1",watcher.summoner.by_id("na1", challenger_Ids[1])['accountId']))
+
+
+#for i in range(1, len(challenger_Ids)):
+#    challenger_accountIds.append(watcher.summoner.by_id(region,challenger_Ids[i])['accountId'])
+
+#for i in range(1,len(masters_Ids)):
+#    masters_accountIds.append(watcher.summoner.by_id(region,masters_Ids[i])['accountId'])
+
+## Now we have Ids, accountIds, and names of each player in challenger and master
+
+#print(challenger_accountIds)
+
+# print(challenger_Ids[1])
+# print(challenger_name[1])
+# print(watcher.summoner.by_id("na1", challenger_Ids[1])['accountId'])
+
+#print(watcher.match.matchlist_by_account_recent("na1",watcher.summoner.by_id("na1", challenger_Ids[1])['accountId']))
